a202604866605de3aa47a67cbf3e9a0d052e3333
Alice Smith <alimay.smith@outlook.com>
2023-01-20 16:11:07 -0700

    testing merge

--- week03_lab/apply_plsql_lab3.sql
+++ week03_lab/apply_plsql_lab3.sql
@@ -8,10 +8,15 @@
 <<<<<<< HEAD
 -- Open log file.
 -- SPOOL apply_plsql_lab3.txt
+<<<<<<< Updated upstream
 =======
 -- Open log file & verify setting
 SPOOL apply_plsql_lab3.txt
 >>>>>>> ef4e9772b2ff54928dcd89057c58b76d6883094f
+=======
+
+-- display settings
+>>>>>>> Stashed changes
 SET SERVEROUTPUT ON SIZE UNLIMITED
 SET VERIFY OFF
 
@@ -24,13 +29,24 @@ DECLARE
     xstring VARCHAR2(30)
  );
 
-  lv_input LIST;
+  lv_input LIST := list();
   lv_three_type THREE_TYPE;
+
+  lv_input1 VARCHAR(100);
+  lv_input2 VARCHAR(100);
+  lv_input3 VARCHAR(100);
 
 BEGIN
   -- get input
-  lv_input := list('&1','&2', '&3');
+  lv_input1 := '&1';
+  lv_input.EXTEND;
+  lv_input(lv_input.COUNT) := lv_input1;
+  lv_input.EXTEND;
+  lv_input(lv_input.COUNT) := '&2';
+  lv_input.EXTEND;
+  lv_input(lv_input.COUNT) := '&3';
   
+
   -- sort and assign input
   FOR i IN 1..lv_input.COUNT LOOP
     -- number
@@ -47,13 +63,14 @@ BEGIN
     END IF;
   END LOOP;
 
+ -- display final result
   dbms_output.put_line('Record ['||lv_three_type.xnum||']'||'['||lv_three_type.xstring||']'||'['||lv_three_type.xdate||']');
 
 END;
 /
 
 -- Close log file.
---SPOOL OFF
+-- SPOOL OFF
 
 -- enable when running through bash script
-QUIT
+ QUIT
