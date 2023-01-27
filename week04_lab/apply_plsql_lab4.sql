0703ab3054894833036ea56704aca1fbece25d9a
Alice Smith <alimay.smith@outlook.com>
2023-01-26 18:38:01 -0700

    initial for week04

---
+++ week04_lab/apply_plsql_lab4.sql
@@ -0,0 +1,26 @@
+/*
+||  Name:          apply_plsql_lab4.sql
+||  Date:          26 Jan 2023
+||  Author:	   Alice Smith
+||  Purpose:       Complete 325 Chapter 5 lab.
+*/
+
+-- Open log file.
+SPOOL log/apply_plsql_lab4.txt
+
+-- display settings
+SET SERVEROUTPUT ON SIZE UNLIMITED
+SET VERIFY OFF
+
+-- Enter your solution here.
+DECLARE
+
+BEGIN
+  dbms_output.put_line('hello World');
+END;
+
+-- Close log file.
+SPOOL OFF
+
+-- enable when running through bash script
+-- QUIT
