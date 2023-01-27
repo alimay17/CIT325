9599be4b92ed2318a7d1ce25e87053146e30d46f
Alice Smith <alimay.smith@outlook.com>
2023-01-26 20:34:56 -0700

    code cleanup and error checks

--- week04_lab/apply_plsql_lab4.sql
+++ week04_lab/apply_plsql_lab4.sql
@@ -1,8 +1,8 @@
 /*
-||  Name:          apply_plsql_lab4.sql
-||  Date:          26 Jan 2023
-||  Author:	   Alice Smith
-||  Purpose:       Complete 325 Chapter 5 lab.
+||  Name:     apply_plsql_lab4.sql
+||  Date:     26 Jan 2023
+||  Author:   Alice Smith
+||  Purpose:  Complete 325 Chapter 5 lab.
 */
 
 -- Open log file.
@@ -13,8 +13,6 @@ SET SERVEROUTPUT ON SIZE UNLIMITED
 SET VERIFY OFF
 SET FEEDBACK OFF
 
--- Enter your solution here.
-
 -- create gift object
 CREATE OR REPLACE TYPE gift IS OBJECT(
   day_name VARCHAR2(8),
@@ -23,7 +21,6 @@ CREATE OR REPLACE TYPE gift IS OBJECT(
 /
 
 DECLARE
-
   -- declare custom types
   TYPE list IS TABLE of VARCHAR2(8);
   TYPE gifts is TABLE of GIFT;
@@ -46,24 +43,22 @@ DECLARE
   ); 
 
 BEGIN
-  -- initial loop
+  -- outer loop
   FOR i IN 1..lv_ordinal_days.COUNT LOOP
     dbms_output.put_line('On the ' || lv_ordinal_days(i) || ' day of Christmas');
     dbms_output.put_line('my true love sent to me:');
-
-    -- second loop
-    FOR x in REVERSE 1..i LOOP
-      -- check for first time loop
+    -- inner loop
+    FOR j in REVERSE 1..i LOOP
+      -- standard condition
       IF i != 1 THEN
-        dbms_output.put_line('-'||lv_gifts(x).day_name||' '|| lv_gifts(x).gift_name);
+        dbms_output.put_line('-'||lv_gifts(j).day_name||' '|| lv_gifts(j).gift_name);
+      -- first time condition
       ELSE
-        dbms_output.put_line('-A ' || lv_gifts(x).gift_name); 
+        dbms_output.put_line('-A ' || lv_gifts(j).gift_name); 
       END IF;  
-    END LOOP;
-
+    END LOOP; -- inner loop
     dbms_output.put_line(CHR(13));
-  END LOOP;
-
+  END LOOP; -- outer loop
 END;
 /
 
