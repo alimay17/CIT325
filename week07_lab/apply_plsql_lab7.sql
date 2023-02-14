989bfe270fc13222db32087a0e808b5512f14146
Alice Smith <alimay.smith@outlook.com>
2023-02-13 17:26:29 -0700

    function stubs in place and tested

--- week07_lab/apply_plsql_lab7.sql
+++ week07_lab/apply_plsql_lab7.sql
@@ -13,27 +13,39 @@ SET SERVEROUTPUT ON SIZE UNLIMITED
 SET VERIFY OFF
 SET FEEDBACK OFF
 
+-- remove insert_contact
+BEGIN
+  FOR i IN (
+    SELECT 
+     uo.object_type,
+     uo.object_name
+    FROM   user_objects uo
+    WHERE  uo.object_name = 'INSERT_CONTACT') LOOP
+    EXECUTE IMMEDIATE 'DROP ' || i.object_type || ' ' || i.object_name;
+  END LOOP;
+END;
+/
 /*
 || Part 1 - All or nothing insert_contact procedure 
-*/
+
 CREATE OR REPLACE PROCEDURE insert_contact(
   pv_first_name VARCHAR2
 ) IS
 
 BEGIN
-  NULL;
+  dbms_output.put_line('this is procedure 1');
 END;
 /
-
+*/
 /*
 || Part 2 - Convert insert_contact procedure from definer to invoker rights 
-*/
+
 CREATE OR REPLACE PROCEDURE insert_contact(
   pv_first_name VARCHAR2
 ) AUTHID CURRENT_USER IS
 
 BEGIN
-  dbms_output.put_line('auth id works' || pv_first_name);
+  dbms_output.put_line('procedure 2 auth id works' || pv_first_name);
 END;
 /
 
@@ -41,25 +53,40 @@ END;
 || Part 3 - Convert insert_contact procedure from invoker to autonomous definer rights function
 || returns NUMBER 1 if success, 0 if not
 */
+-- Drop procedure to run function
+-- DROP PROCEDURE insert_contact;
 CREATE OR REPLACE FUNCTION insert_contact(
   pv_first_name VARCHAR2
-) RETURN NUMBER IS
+) RETURN NUMBER AUTHID DEFINER IS
 
-lv_return := 1;
+ lv_return NUMBER := 1;
 BEGIN
-  RETURN lv_return;
-END;
+  dbms_output.put_line('this is function 1: '||pv_first_name || ' Return value: ' || lv_return);
+  RETURN 1;
+END insert_contact;
 /
 
 /*
 || Part 4 - get_contact object table function using a contact_obj, contact_tab setup 
 || returns complete list of persons format: 'first, middle, last'
-*/
+
 CREATE OR REPLACE FUNCTION get_contact (
   pv_first_name VARCHAR2
 ) RETURN CONTACT_TAB IS
 BEGIN
   NULL;
+END;
+/
+*/
+/*
+|| testing
+*/
+DECLARE
+  lv_num NUMBER;
+BEGIN
+--  insert_contact('firstName');
+  lv_num := insert_contact('firstName');
+  dbms_output.put_line(lv_num);
 END;
 /
 -- Close log file.
