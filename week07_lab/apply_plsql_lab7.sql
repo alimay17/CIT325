65f95322331e917e6c556a6490f0ed03ba28e5d2
Alice Smith <alimay.smith@outlook.com>
2023-02-13 20:10:24 -0700

    full test 1

--- week07_lab/apply_plsql_lab7.sql
+++ week07_lab/apply_plsql_lab7.sql
@@ -29,7 +29,7 @@ END;
 || Part 1 - All or nothing insert_contact procedure 
 */
 CREATE OR REPLACE PROCEDURE insert_contact(
-  /* pv_first_name         VARCHAR2,
+  pv_first_name         VARCHAR2,
   pv_middle_name        VARCHAR2,
   pv_last_name          VARCHAR2,
   pv_contact_type       VARCHAR2,
@@ -44,18 +44,18 @@ CREATE OR REPLACE PROCEDURE insert_contact(
   pv_country_code       VARCHAR2,
   pv_area_code          VARCHAR2,
   pv_telephone_number   VARCHAR2,
-  pv_telephone_type     VARCHAR2,*/
+  pv_telephone_type     VARCHAR2,
   pv_user_name          VARCHAR2
 ) IS
 
   -- local variables
-  /* lv_address_id     VARCHAR2(30);
-  lv_contact_id     VARCHAR2(30);
-  lv_credit_card_id VARCHAR2(30);
-  lv_member_id      VARCHAR2(30);
-  lv_telephone_id   VARCHAR2(30); */
-  lv_sys_user_id    VARCHAR2(30);
-  lv_time_stamp     DATE;
+  lv_address_id_type     VARCHAR2(30);
+  lv_contact_id_type     VARCHAR2(30);
+  lv_credit_card_id_type VARCHAR2(30);
+  lv_member_id_type      VARCHAR2(30);
+  lv_telephone_id_type   VARCHAR2(30);
+  lv_created_by          VARCHAR2(30);
+  lv_time_stamp DATE := SYSDATE;
 
   -- get_lookup_id function
   FUNCTION get_lookup_id(
@@ -90,55 +90,170 @@ CREATE OR REPLACE PROCEDURE insert_contact(
   END get_lookup_id;
 
 -- begin main function insert_contact
-BEGIN
-  /* lv_address_id := get_lookup_id(
-    'ADDRESS',
-    'ADDRESS_TYPE',
-    pv_address_type
-  ); */
+BEGIN 
 
-  SELECT system_user_id INTO lv_sys_user_id
+  -- get lookup id's
+  lv_member_id_type := get_lookup_id('MEMBER','MEMBER_TYPE',pv_member_type);
+  lv_credit_card_id_type := get_lookup_id('MEMBER','CREDIT_CARD_TYPE',pv_credit_card_type);
+  lv_contact_id_type := get_lookup_id('CONTACT','CONTACT_TYPE',pv_contact_type);
+  lv_address_id_type := get_lookup_id('ADDRESS','ADDRESS_TYPE',pv_address_type);
+  lv_telephone_id_type := get_lookup_id('TELEPHONE','TELEPHONE_TYPE',pv_telephone_type);
+
+  -- get system user and timestamp
+  SELECT system_user_id INTO lv_created_by
     FROM system_user
     WHERE system_user_name = pv_user_name;
 
-  SELECT SYSDATE INTO lv_time_stamp FROM DUAL;
-
   SAVEPOINT starting_point;
  
-  /* INSERT INTO member (
+  -- insert values
+  -- member
+  INSERT INTO member (
     member_id,
     member_type,
     account_number,
     credit_card_number,
     credit_card_type,
     created_by,
+    creation_date,
     last_updated_by,
     last_update_date
   ) VALUES (
     member_s1.NEXTVAL,
-    lv_member_id,
+    lv_member_id_type,
     pv_account_number,
     pv_credit_card_number,
-    lv_credit_card_id,
-    lv_sys_user_id,
+    lv_credit_card_id_type,
+    lv_created_by,
     lv_time_stamp,
-    lv_sys_user_id,
+    lv_created_by,
     lv_time_stamp
-  ); */
+  );
 
-  dbms_output.put_line(lv_time_stamp || ' this is the timeStamp');
+  -- contact
+  INSERT INTO contact(
+    contact_id,
+    member_id,
+    contact_type,
+    last_name,
+    first_name,
+    middle_name,
+    created_by,
+    creation_date,
+    last_updated_by,
+    last_update_date
+  ) VALUES (
+    contact_s1.NEXTVAL,
+    member_s1.CURRVAL,
+    lv_contact_id_type,
+    pv_last_name,
+    pv_first_name,
+    pv_middle_name,
+    lv_created_by,
+    lv_time_stamp,
+    lv_created_by,
+    lv_time_stamp
+  );
 
-END;
+  -- address
+  INSERT INTO address VALUES (
+    address_s1.NEXTVAL,
+    contact_s1.CURRVAL,
+    lv_address_id_type,
+    pv_city,
+    pv_state_province,
+    pv_postal_code,
+    lv_created_by,
+    lv_time_stamp,
+    lv_created_by,
+    lv_time_stamp
+  );
+
+  -- street_address
+  INSERT INTO street_address VALUES(
+    street_address_s1.NEXTVAL,
+    address_s1.CURRVAL,
+    1,
+    pv_street_address,
+    lv_created_by,
+    lv_time_stamp,
+    lv_created_by,
+    lv_time_stamp
+  );
+
+  -- telephone
+  INSERT INTO telephone VALUES (
+    telephone_s1.NEXTVAL,
+    contact_s1.CURRVAL,
+    address_s1.CURRVAL
+    lv_telephone_id_type,
+    pv_country_code,
+    pv_area_code,
+    pv_telephone_number,
+    lv_created_by,
+    lv_time_stamp,
+    lv_created_by,
+    lv_time_stamp
+  );
+
+  COMMIT;
+
+-- EXCEPTION 
+--   WHEN OTHERS THEN
+--     ROLLBACK TO starting_point;
+-- RETURN;
+
+END insert_contact;
 /
 
+SHOW ERRORS
 /*
 || Part 1
 || TEST BLOCk 
 */
 BEGIN
-  insert_contact('DBA 2');
+  NULL;
+  insert_contact(
+    'Charles',
+    'Francis',
+    'Xavier',
+    'CUSTOMER',
+    'SLC-000008',
+    'INDIVIDUAL',
+    '7777-6666-5555-4444',
+    'DISCOVER_CARD',
+    'Milbridge',
+    'Maine',
+    'HOME',
+    '001',
+    '207',
+    '111-1234',
+    'HOME',
+    'DBA 2'
+  );
 END;
 /
+
+-- test query
+COL full_name      FORMAT A24
+COL account_number FORMAT A10 HEADING "ACCOUNT|NUMBER"
+COL address        FORMAT A22
+COL telephone      FORMAT A14
+
+SELECT c.first_name
+||     CASE
+         WHEN c.middle_name IS NOT NULL THEN ' '||c.middle_name||' ' ELSE ' '
+       END
+||     c.last_name AS full_name
+,      m.account_number
+,      a.city || ', ' || a.state_province AS address
+,      '(' || t.area_code || ') ' || t.telephone_number AS telephone
+FROM   member m INNER JOIN contact c
+ON     m.member_id = c.member_id INNER JOIN address a
+ON     c.contact_id = a.contact_id INNER JOIN telephone t
+ON     c.contact_id = t.contact_id
+AND    a.address_id = t.address_id
+WHERE  c.last_name = 'Xavier';
 
 /*
 || Part 2 - Convert insert_contact procedure from definer to invoker rights 
