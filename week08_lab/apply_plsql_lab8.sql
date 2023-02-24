29f20637e15f1b11526053f1e6f6e067a09e3dd6
Alice Smith <alimay.smith@outlook.com>
2023-02-24 16:00:34 -0700

    initial file for week08 lab

---
+++ week08_lab/apply_plsql_lab8.sql
@@ -0,0 +1,14 @@
+/*
+||  Name:          apply_plsql_lab8.sql
+||  Date:          24 Feb 2023
+||  Author:        Alice Smith
+||  Purpose:       Complete 325 Chapter 9 lab.
+*/
+
+-- Open log file.
+SPOOL apply_plsql_lab8.txt
+
+-- Enter your solution here.
+
+-- Close log file.
+SPOOL OFF
