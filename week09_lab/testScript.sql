daba92d237c82d5a5aa222199b5b89a33740dd6b
Alice Smith <alimay.smith@outlook.com>
2023-02-28 11:23:08 -0700

    added test script

---
+++ week09_lab/testScript.sql
@@ -0,0 +1,15 @@
+/*
+||  Name:          testScript.sql
+||  Date:          28 Feb 2023
+||  Purpose:       Test components seperatly before adding to full
+*/
+
+-- logfile
+SPOOL 'logs/testScript.txt'
+
+-- environment settings
+SET SERVEROUTPUT ON SIZE UNLIMITED
+SET VERIFY OFF
+
+
+SPOOL OFF
