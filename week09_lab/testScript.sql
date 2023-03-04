b9a7c9a3633b3051a144af8a709a3762bca082d9
Alice Smith <alimay.smith@outlook.com>
2023-03-03 18:53:02 -0700

    done

--- week09_lab/testScript.sql
+++ week09_lab/testScript.sql
@@ -11,74 +11,55 @@ SPOOL 'logs/testScript.txt'
 SET SERVEROUTPUT ON SIZE UNLIMITED
 SET VERIFY OFF
 
-CREATE OR REPLACE PROCEDURE load_clob_from_file( 
-    src_file_name     IN VARCHAR2,
-    table_name        IN VARCHAR2, 
-    column_name       IN VARCHAR2,
-    primary_key_name  IN VARCHAR2,
-    primary_key_value IN VARCHAR2 
-) IS
+DROP PROCEDURE update_item_description;
 
-    -- Define local variables for DBMS_LOB.LOADCLOBFROMFILE procedure.
-    des_clob   CLOB;
-    src_clob   BFILE := BFILENAME('UPLOADTEXT',src_file_name);
-    des_offset NUMBER := 1;
-    src_offset NUMBER := 1;
-    ctx_lang   NUMBER := dbms_lob.default_lang_ctx;
-    warning    NUMBER;
+CREATE OR REPLACE PROCEDURE update_item_description(pv_item_title VARCHAR2) IS
 
-    -- Define a pre-reading size.
-    src_clob_size NUMBER;
+  CURSOR get_item(cv_lookup_string VARCHAR2) IS
+    SELECT i.item_id, i.item_title, i.text_file_name
+    FROM item i
+    INNER JOIN file_list f ON f.file_name = i.text_file_name
+    WHERE i.item_title LIKE '%' || cv_lookup_string || '%';
 
-    -- Define local variable for Native Dynamic SQL.
-    stmt VARCHAR2(2000);
+  BEGIN
 
-    BEGIN
-
-    -- Opening source file is a mandatory operation.
-    IF dbms_lob.fileexists(src_clob) = 1 AND NOT dbms_lob.isopen(src_clob) = 1 THEN
-        src_clob_size := dbms_lob.getlength(src_clob);
-        dbms_lob.OPEN(src_clob,DBMS_LOB.LOB_READONLY);
-    END IF;
-
-    -- Assign dynamic string to statement.
-    stmt := 'UPDATE '||table_name||' '
-        || 'SET    '||column_name||' = empty_clob() '
-        || 'WHERE  '||primary_key_name||' = '||''''||primary_key_value||''' '
-        || 'RETURNING '||column_name||' INTO :locator';
-
-    -- Run dynamic statement.
-    EXECUTE IMMEDIATE stmt USING OUT des_clob;
-
-    -- Read and write file to CLOB, close source file and commit.
-    dbms_lob.loadclobfromfile( 
-        dest_lob     => des_clob,
-        src_bfile    => src_clob,
-        amount       => dbms_lob.getlength(src_clob),
-        dest_offset  => des_offset,
-        src_offset   => src_offset,
-        bfile_csid   => dbms_lob.default_csid,
-        lang_context => ctx_lang, 
-        warning      => warning 
+  FOR i IN get_item(pv_item_title) LOOP
+    load_clob_from_file(
+      src_file_name     => i.text_file_name,
+      table_name        => 'ITEM',
+      column_name       => 'ITEM_DESC',
+      primary_key_name  => 'ITEM_ID',
+      primary_key_value => TO_CHAR(i.item_id)
     );
+  END LOOP;
 
-    -- Close open source file.
-    dbms_lob.CLOSE(src_clob);
+  -- handle exceptions
+  EXCEPTION 
+    WHEN OTHERS THEN
+      dbms_output.put_line('Unable to update descriptions');
 
-    -- Commit write and conditionally acknowledge it.
-    IF src_clob_size = dbms_lob.getlength(des_clob) THEN
-        $IF $$DEBUG = 1 $THEN
-        dbms_output.put_line('Success!');
-        $END
-        COMMIT;
-    ELSE
-        $IF $$DEBUG = 1 $THEN
-        dbms_output.put_line('Failure.');
-        $END
-        RAISE dbms_lob.operation_failed;
-    END IF;
-
-END load_clob_from_file;
+END;
 /
 
+-- test 
+EXECUTE update_item_description('Harry Potter');
+
+COL item_id     FORMAT 9999
+COL item_title  FORMAT A44
+COL text_size   FORMAT 999,999
+SET PAGESIZE 99
+SELECT   item_id
+,        item_title
+,        LENGTH(item_desc) AS text_size
+FROM     item
+WHERE    REGEXP_LIKE(item_title,'^Harry Potter.*$')
+AND      item_type IN
+        (SELECT common_lookup_id
+        FROM   common_lookup
+        WHERE  common_lookup_table = 'ITEM'
+        AND    common_lookup_column = 'ITEM_TYPE'
+        AND    REGEXP_LIKE(common_lookup_type,'^(dvd|vhs).*$','i'))
+ORDER BY item_id;
+
+-- LIST
 SPOOL OFF
