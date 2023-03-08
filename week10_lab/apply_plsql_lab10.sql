b026f19e434b92aaa6f0f2c84b739935206e6577
Alice Smith <alimay.smith@outlook.com>
2023-03-08 10:03:08 -0700

    ready for submission

--- week10_lab/apply_plsql_lab10.sql
+++ week10_lab/apply_plsql_lab10.sql
@@ -98,6 +98,51 @@ BEGIN
 END;
 /
 
+/* create logger table and sequence */
+CREATE TABLE logger(
+    logger_id NUMBER,
+    log_text BASE_T
+);
+CREATE SEQUENCE logger_s;
+
+-- check table creation
+DESC logger;
+
+-- insert new base_t row default constuctor
+INSERT INTO logger VALUES(
+    logger_s.NEXTVAL,
+    base_t() 
+);
+-- insert new base_t with parameter constructor
+DECLARE
+    lv_base_t BASE_T;
+BEGIN
+    lv_base_t := base_t(
+        oname => 'BASE_T',
+        name => 'NEW'
+    );
+    INSERT INTO logger VALUES(
+        logger_s.NEXTVAL,
+        lv_base_t
+    );
+    COMMIT;
+END;
+/
+
+-- test base_t insertion
+COLUMN oname     FORMAT A20
+COLUMN get_name  FORMAT A20
+COLUMN to_string FORMAT A20
+SELECT t.logger_id
+,      t.log.oname AS oname
+,      NVL(t.log.get_name(),'Unset') AS get_name
+,      t.log.to_string() AS to_string
+FROM  (SELECT l.logger_id
+       ,      TREAT(l.log_text AS base_t) AS log
+       FROM   logger l) t
+WHERE  t.log.oname = 'BASE_T';
+
+
 /* Create subtype item_t */
 -- subtype item_t declaration
 CREATE OR REPLACE TYPE item_t UNDER base_t (
@@ -199,7 +244,7 @@ END;
 -- verify item_t
 DESC item_t
 
-/* Create subtype item_t */
+/* Create subtype contact_t */
 -- subtype contact_t declaration
 CREATE OR REPLACE TYPE contact_t UNDER base_t (
     -- properties unique to subtype
@@ -236,7 +281,7 @@ CREATE OR REPLACE TYPE contact_t UNDER base_t (
 ) INSTANTIABLE NOT FINAL;
 /
 
--- subtype item_t body
+-- subtype contact_t body
 CREATE OR REPLACE TYPE BODY contact_t IS
 
     -- constructors
@@ -292,25 +337,58 @@ END;
 --verify contact_t
 DESC contact_t
 
-/* create logger table and sequence */
-CREATE TABLE logger(
-    logger_id NUMBER,
-    log_text BASE_T
-);
-CREATE SEQUENCE logger_s;
 
--- check table creation
-DESC logger;
+-- insert new subclass rows with parameter constructors
+DECLARE
+    lv_item_t ITEM_T;
+    lv_contact_t CONTACT_T;
+BEGIN
+    -- create new item_t object
+    lv_item_t := item_t(
+        oname => 'ITEM_T',
+        name => 'NEW',
+        item_id => 1212,
+        item_barcode => '',
+        item_type => 0,
+        item_title => 'Test Item',
+        item_subtitle => '',
+        item_rating => 'G',
+        item_rating_agency => '',
+        item_release_date => SYSDATE,
+        created_by => 0,
+        creation_date => SYSDATE,
+        last_updated_by => 0,
+        last_updated_date => SYSDATE
+    );
 
-/* Fill in Logger table */
+    -- create new contact_t object
+    lv_contact_t := contact_t(
+        oname => 'CONTACT_T',
+        name => 'NEW',
+        contact_id => 0,
+        member_id => 0,
+        contact_type => 0,
+        first_name => 'A',
+        middle_name => '',
+        last_name => 'S',
+        created_by => 0,
+        creation_date => SYSDATE,
+        last_updated_by => 0,
+        last_updated_date => SYSDATE
+    );
 
--- new base_t row default constuctor
-INSERT INTO logger VALUES(
-    logger_s.NEXTVAL,
-    base_t() 
-);
-
--- new base_t row parameter constructor
+    -- insert new objects
+    INSERT INTO logger VALUES(
+        logger_s.NEXTVAL,
+        lv_item_t
+    );
+    INSERT INTO logger VALUES(
+        logger_s.NEXTVAL,
+        lv_contact_t
+    );
+    COMMIT;
+END;
+/
 
 -- test logger table insertions
 COLUMN oname     FORMAT A20
@@ -323,9 +401,7 @@ SELECT t.logger_id
 FROM  (SELECT l.logger_id
        ,      TREAT(l.log_text AS base_t) AS log
        FROM   logger l) t
-WHERE  t.log.oname IN ('BASE_T','CONTACT_T','ITEM_T');
--- list
--- SHOW ERRORS
+WHERE  t.log.oname IN ('CONTACT_T','ITEM_T');
 
 -- Close log file.
 SPOOL OFF
