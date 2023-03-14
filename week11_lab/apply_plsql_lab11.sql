436564f29217ab53698bf8544b2f8a22ccb80d77
Alice Smith <alimay.smith@outlook.com>
2023-03-14 11:02:44 -0600

    initial files for week11

---
+++ week11_lab/apply_plsql_lab11.sql
@@ -0,0 +1,18 @@
+/*
+||  Name:          apply_plsql_lab11.sql
+||  Date:          14 Mar 2023
+||  Purpose:       Complete 325 Chapter 12 lab.
+||  Dependencies:  Run the Oracle Database 12c PL/SQL Programming setup programs.
+*/
+
+/* Call seeding libraries */
+@$LIB/cleanup_oracle.sql
+@$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+
+-- Open log file.
+SPOOL apply_plsql_lab11.txt
+
+-- ... insert your solution here ...
+
+-- Close log file.
+SPOOL OFF
