025b4c645ca343b81dc88bb14300364016d7d6a9
Alice Smith <alimay.smith@outlook.com>
2023-03-28 10:43:58 -0600

    base_t done

--- final_project/types/base_t.sql
+++ final_project/types/base_t.sql
@@ -4,8 +4,7 @@
 ||  Purpose:      Create base type for final project
 ||  Author:       Alice Smith  
 */
-
-DROP type base_t;
+SPOOL 'logs/base_t.txt'
 
 /* Create new base object */
 -- base_t declaration
@@ -31,51 +30,44 @@ CREATE OR REPLACE TYPE base_t IS OBJECT(
 -- end base_t declaration
 /
 
-LIST;
-SHOW ERRORS;
 
 -- base_t object body
--- CREATE OR REPLACE TYPE BODY base_t IS
+CREATE OR REPLACE TYPE BODY base_t IS
 
---     -- constructors
---     CONSTRUCTOR FUNCTION base_t RETURN SELF AS RESULT IS
---         BEGIN
---             self.oname := 'BASE_T';
---         RETURN;
---     END;
+    -- constructor
+    CONSTRUCTOR FUNCTION base_t(
+        oid   NUMBER,
+        oname VARCHAR2
+    ) RETURN SELF AS RESULT IS
+        BEGIN
+            self.oid    := oid;
+            self.oname  := oname;
+        RETURN;
+    END;
 
---     CONSTRUCTOR FUNCTION base_t(
---         oname VARCHAR2,
---         name VARCHAR2
---     ) RETURN SELF AS RESULT IS
---         BEGIN
---             IF name IS NOT NULL AND name IN ('NEW','OLD') THEN
---                 self.name := name;
---             END IF;
---         RETURN;
---     END;
+    -- getters and setters
+    MEMBER FUNCTION get_oname RETURN VARCHAR2 IS
+        BEGIN
+        RETURN self.oname;
+    END get_oname;
 
---     -- getters and setters
---     MEMBER FUNCTION get_oname RETURN VARCHAR2 IS
---         BEGIN
---         RETURN self.oname;
---     END get_oname;
+    MEMBER FUNCTION get_name RETURN VARCHAR2 IS
+        BEGIN
+        RETURN NULL;
+    END get_name;
 
---     MEMBER FUNCTION get_name RETURN VARCHAR2 IS
---         BEGIN
---         RETURN self.name;
---     END get_name;
+    MEMBER PROCEDURE set_oname(oname VARCHAR2) IS
+        BEGIN
+            self.oname := oname;
+    END set_oname;
 
---     MEMBER PROCEDURE set_oname(oname VARCHAR2) IS
---         BEGIN
---             self.oname := oname;
---     END set_oname;
-
---     -- other
---     MEMBER FUNCTION to_string RETURN VARCHAR2 IS
---         BEGIN
---         RETURN '['||self.oname||']';
---     END to_string;
--- END;
--- /
--- end base_t body
+    -- other
+    MEMBER FUNCTION to_string RETURN VARCHAR2 IS
+        BEGIN
+        RETURN '['||self.oid||']['||self.oname||']';
+    END to_string;
+END;
+/
+-- end base_t body;
+SPOOL OFF
+QUIT
