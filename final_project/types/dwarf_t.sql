0e0ba01aa1e91f8cecf7a3fc371a40ae9a5a5ede
Alice Smith <alimay.smith@outlook.com>
2023-03-29 10:49:59 -0600

    type declaration of dwarf

--- final_project/types/dwarf_t.sql
+++ final_project/types/dwarf_t.sql
@@ -1,6 +1,31 @@
 /*
 ||  Name:         base_t.sql
 ||  Date:         28 Mar 2023
-||  Purpose:      Create base type for final project
+||  Purpose:      Create dwarf subtype
 ||  Author:       Alice Smith  
 */
+
+CREATE OR REPLACE TYPE dwarf_t UNDER base_t (
+    -- properties unique to subtype
+    name  VARCHAR2(30),
+    genus VARCHAR2(30),
+
+    -- subtype constructor
+    CONSTRUCTOR FUNCTION dwarf_t(
+        name  VARCHAR2,
+        genus VARCHAR2
+    ) RETURN SELF AS RESULT,
+
+    -- overriding members
+    OVERRIDING MEMBER FUNCTION get_name RETURN VARCHAR2,
+
+    -- custom members
+    MEMBER FUNCTION get_genus RETURN VARCHAR2,
+    MEMBER PROCEDURE set_genus (genus VARCHAR2),
+    MEMBER PROCEDURE set_name (name VARCHAR2)
+) INSTANTIABLE NOT FINAL;
+/
+
+list;
+SHOW ERRORS;
+
